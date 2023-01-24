46b981f2eabe364262794a9537caf10e72543dfa
Drew Sarette <103331340+drew-sarette@users.noreply.github.com>
2023-01-24 14:05:38 -0500

    Read saved data for daily servings from localStorage

--- js/index.js
+++ js/index.js
@@ -1,26 +1,22 @@
 const defaultSettings = [
-  { name:"grains", jsVariable: "grains", htmlID: "grains", timeFrame: "daily", servings: 8, step: 1, color: "#996633" },
+  { name:"grains", jsVariable: "grains", timeFrame: "daily", servings: 8, step: 1, color: "#996633" },
   { name:"vegetables", jsVariable: "vegetables", htmlID: "vegetables", timeFrame: "daily", servings: 5, step: 1, color: "#00cc00" },
-  { name:"fruits", jsVariable: "fruits", htmlID: "fruits", timeFrame: "daily", servings: 5, step: 1, color: "#ffff00" },
-  { name:"meat", jsVariable: "meat", htmlID: "meat", timeFrame: "daily", servings: 6, step: 1, color: "#cc0000" },
-  { name:"dairy", jsVariable: "dairy", htmlID: "dairy", timeFrame: "daily", servings: 3, step: 1, color: "#ffffcc" },
-  { name:"fats & oils", jsVariable: "fatsOils", htmlID: "fats-oils", timeFrame: "daily", servings: 3, step: 1, color: "#ff66ff" },
-  { name:"sodium", jsVariable: "sodium", htmlID: "sodium", timeFrame: "daily", servings: 2300, step: 100, color: "#00ffff" },
-  { name:"caffeine", jsVariable: "caffeine", htmlID: "caffeine", timeFrame: "daily", servings: 200, step: 20, color: "#ff6666" },
-  { name:"sweets", jsVariable: "sweets", htmlID: "sweets", timeFrame: "weekly", servings: 4, step: 1, color: "#993366" },
-  { name:"nuts & legumes", jsVariable: "nutsSeedsLegumes", htmlID: "nuts-seeds-legumes", timeFrame: "weekly", servings: 4, step: 1, color: "#006699" },
-  { name:"alcohol", jsVariable: "alcohol", htmlID: "alcohol", timeFrame: "weekly", servings: 4, step: 1, color: "#ff6666" }
+  { name:"fruits", jsVariable: "fruits", timeFrame: "daily", servings: 5, step: 1, color: "#ffff00" },
+  { name:"meat", jsVariable: "meat", timeFrame: "daily", servings: 6, step: 1, color: "#cc0000" },
+  { name:"dairy", jsVariable: "dairy", timeFrame: "daily", servings: 3, step: 1, color: "#ffffcc" },
+  { name:"fats & oils", jsVariable: "fatsOils", timeFrame: "daily", servings: 3, step: 1, color: "#ff66ff" },
+  { name:"sodium", jsVariable: "sodium", timeFrame: "daily", servings: 2300, step: 100, color: "#00ffff" },
+  { name:"caffeine", jsVariable: "caffeine", timeFrame: "daily", servings: 200, step: 20, color: "#ff6666" },
+  { name:"sweets", jsVariable: "sweets", timeFrame: "weekly", servings: 4, step: 1, color: "#993366" },
+  { name:"nuts & legumes", jsVariable: "nutsSeedsLegumes", timeFrame: "weekly", servings: 4, step: 1, color: "#006699" },
+  { name:"alcohol", jsVariable: "alcohol", timeFrame: "weekly", servings: 4, step: 1, color: "#ff6666" }
 ];
-
-// window.addEventListener("beforeunload", function() {
-//   document.localStorage.setItem("test", "hi");
-// });
 
 (function () {
   let settings = JSON.parse(localStorage.getItem("settings"));
   if (!settings) {settings = defaultSettings};
   displayServingCounters(settings);
-  loadTodayData(settings);
+  loadTodayData();
 })();
 
 function displayServingCounters(settings) {
@@ -39,7 +35,7 @@ function createCounter(sObj) {
   counter.step = sObj.step;
   counter.color = sObj.color;
   counter.name = sObj.name;
-  counter.id = sObj.htmlID;
+  counter.id = sObj.jsVariable;
   const icon = document.createElement("img");
   icon.src = `img/${sObj.jsVariable}.png`;
   icon.slot = "icon";
@@ -48,13 +44,38 @@ function createCounter(sObj) {
   return counter;
 }
 
-function loadTodayData(settings) {
+function loadTodayData() {
+  const today = JSON.parse(localStorage.getItem("today"));
+  console.log(today);
+  if (today) {
+    for (const key in today) {
+      document.getElementById(key).current = today[key];
+    }
+  }
 
 }
 
 function saveData(ev) {
   const startFromZero = {grains: 0, fruits: 0, vegetables: 0, meat: 0, dairy: 0, fatsOils: 0, sodium: 0, caffeine: 0, nutsSeedsLegumes: 0, sweets: 0, alcohol: 0}
   const today = JSON.parse(localStorage.getItem("today")) ?? startFromZero;
-  today[ev.target.counts] = ev.target.current;
-  localStorage.setItem("today", JSON.stringify(today));
+  let currentDate = new Date().toDateString();
+  if (today.date != currentDate && today?.date) {
+    startNewDay(today);
+  }
+  else {
+    today[ev.target.counts] = ev.target.current;
+    today.date = currentDate;
+    localStorage.setItem("today", JSON.stringify(today));
+  }
+}
+
+function startNewDay(oldDay) {
+  let days = JSON.parse(localStorage.getItem("days"));
+  if (days){
+    console.log(typeof days)
+  }
+  else {
+    days = [oldDay]
+  }
+  localStorage.setItem("days", JSON.stringify(days));
 }

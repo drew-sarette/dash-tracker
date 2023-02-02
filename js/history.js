b4b193cc7f4436cfa1848de2e25530fce857282d
Drew Sarette <103331340+drew-sarette@users.noreply.github.com>
2023-02-01 22:39:47 -0500

    History js functional

--- js/history.js
+++ js/history.js
@@ -1,12 +1,12 @@
 const mainContent = document.querySelector("main");
 let days = JSON.parse(localStorage.getItem("days"));
 let weeksWithDays = JSON.parse(localStorage.getItem("weeks"));
-
 if (days && weeksWithDays) {
-
+  
   weeksWithDays.sort(mostRecentFirst);
   days.sort(mostRecentFirst);
   let weekNum = weeksWithDays.length;
+  
   weeksWithDays.forEach(w => {
     w.number = weekNum;
     weekNum--;
@@ -17,11 +17,16 @@ if (days && weeksWithDays) {
     { dayNo: 5, date: null, data: null },
     { dayNo: 6, date: null, data: null },
     { dayNo: 7, date: null, data: null }];
+    
     w.days.forEach(wd => {
-      wd.date = new Date(new Date(w.date).getTime() + (wd.dayNo - 1) * 24 * 60 * 60 * 1000).setHours(0, 0, 0, 0);
-      wd.data = days.find(d => Date.getTime(new Date(d.date).setHours(0, 0, 0, 0)) === wd.date.getTime());
-      console.log("hi");
-      console.log(`Week: ${w.number} dayNo: ${wd.dayNo} data: ${wd.data}`);
+      wd.date = new Date(w.date).getTime() + (wd.dayNo - 1) * 24 * 60 * 60 * 1000;
+      wd.date = new Date(wd.date);
+      wd.date.setHours(0,0,0,0);
+      wd.data = days.find(d => {
+        const t = new Date(d.date);
+        t.setHours(0,0,0,0);
+        return (t.getTime()  === wd.date.getTime());
+      });
     });
   });
 }
@@ -29,6 +34,7 @@ else {
   mainContent.textContent = "No data found";
 }
 console.log(weeksWithDays);
+
 function mostRecentFirst(a, b) {
   // sorts biggest (most recent) values first
   let [dateA, dateB] = [new Date(a.date).getTime(), new Date(b.date).getTime()];

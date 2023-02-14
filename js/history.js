d9b213cce4d4ba8d75e6a540b1410d89be3fe22f
Drew Sarette <103331340+drew-sarette@users.noreply.github.com>
2023-02-14 16:54:34 -0500

    Update current week with today's data before displaying

--- js/history.js
+++ js/history.js
@@ -4,13 +4,16 @@ import { settingsRepo } from "./settings-repo.js";
 const settings = settingsRepo.getSettings();
 const mainContent = document.querySelector("main");
 let weeks = JSON.parse(localStorage.getItem("weeks"));
-if (weeks) {
-  weeks.forEach(w => {
+let today = JSON.parse(localStorage.getItem("today"));
+if (today && weeks) {
+  weeks[0].days.find(
+    (d) => datesRepo.compareDateArrs(d.date, today.date) === 0
+  ).data = today.data;
+  weeks.forEach((w) => {
     const weekComponent = createWeekComponent(w);
     mainContent.appendChild(weekComponent);
-  })
-}
-else {
+  });
+} else {
   mainContent.textContent = "No data found";
 }
 
@@ -19,17 +22,19 @@ function createWeekComponent(w) {
   h3.textContent = `Week of ${w.date[1]}-${w.date[2]}-${w.date[0]}`;
   h3.slot = "heading";
   const weekData = [];
-  for (const key in w.data) {weekData.push(makeDataItem(key, w.data))}
+  for (const key in w.data) {
+    weekData.push(makeDataItem(key, w.data));
+  }
   const dayComponents = [];
-  w.days.forEach(d => dayComponents.push(createDayComponent(d)));
+  w.days.forEach((d) => dayComponents.push(createDayComponent(d)));
   const weekComponent = document.createElement("week-component");
-  weekComponent.append(h3,...weekData, ...dayComponents)
+  weekComponent.append(h3, ...weekData, ...dayComponents);
   return weekComponent;
 }
 
 function makeDataItem(key, dataObj) {
   const s = settingsRepo.getSettings();
-  const si = s.find( item => item.jsVariable === key);
+  const si = s.find((item) => item.jsVariable === key);
   const p = document.createElement("p");
   p.textContent = `${si.name}: ${dataObj[key]} of ${si.servings}`;
   p.slot = "week-data";
@@ -37,13 +42,21 @@ function makeDataItem(key, dataObj) {
 }
 
 function createDayComponent(d) {
-  const [dayComponent, h4, fgs] = [document.createElement("day-component"), document.createElement("h4"), []];
+  const [dayComponent, h4, fgs] = [
+    document.createElement("day-component"),
+    document.createElement("h4"),
+    [],
+  ];
   dayComponent.slot = "days";
   h4.slot = "day-date";
   h4.textContent = `${d.date[1]}-${d.date[2]}`;
   for (const foodGroup in d.data) {
     const setting = settingsRepo.findSetting(foodGroup);
-    const [icon, info, container] = [document.createElement("img"), `${setting.name}: ${d.data[foodGroup]} of ${setting.servings}`, document.createElement("div")];
+    const [icon, info, container] = [
+      document.createElement("img"),
+      `${setting.name}: ${d.data[foodGroup]} of ${setting.servings}`,
+      document.createElement("div"),
+    ];
     icon.src = `/img/${foodGroup}.png`;
     if (Math.abs(d.data[foodGroup] - setting.servings) <= 1) {
       icon.style.backgroundColor = setting.color;
@@ -52,6 +65,6 @@ function createDayComponent(d) {
     container.append(icon, info);
     fgs.push(container);
   }
-  dayComponent.append(h4,...fgs);
+  dayComponent.append(h4, ...fgs);
   return dayComponent;
 }

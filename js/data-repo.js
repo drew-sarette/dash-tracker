8a4ecfc3914acf1e65d81a83a9a5a74ed1704b54
Drew Sarette <103331340+drew-sarette@users.noreply.github.com>
2023-03-13 21:19:37 -0400

    Add testing data, simplify getLastWeek

--- js/data-repo.js
+++ js/data-repo.js
@@ -1,5 +1,4 @@
 import datesRepo from "./dates-repo.js";
-import testDays from "./testDays.js";
 
 const dataRepo = {
     getDays: function () {
@@ -13,29 +12,13 @@ const dataRepo = {
 
     getLastWeek: function () {
         let days = this.getDays() ? this.getDays() : [];
-        days = testDays.reverse();
         const last7entries = days.slice(0, 7);
         //startDate is the day six days ago
         const startDate = datesRepo.addDaysToDate(datesRepo.justDate(), -5);
-        const week = [];
-        last7entries.forEach(entry => {
-            if (datesRepo.compareDateArrs(startDate, entry.date) >= 0) {
-                week.push(entry);
-            }
-        })
-        console.log(week);
-        return week;
+        return last7entries.filter( entry => {
+            return datesRepo.compareDateArrs(startDate, entry.date) >= 0;
+        });
     },
-
-    getAllWeeks: function() {
-        const weeks = [];
-        const days = this.getDays() ? this.getDay() : [];
-        for (let i = 0; i<7; i++){
-            const nextDate = datesRepo.addDaysToDate(newWeek.date, i)
-            newWeek.days.push(createNewDay(nextDate));
-        };
-
-    }
 }
 
 export default dataRepo;
---
+++ js/testDays.js
@@ -0,0 +1,212 @@
+const days = [
+    {
+      date: [2023, 3, 1],
+      data: {
+        grains: 3,
+        vegetables: 2,
+        fruits: 2,
+        meat: 6,
+        dairy: 2,
+        fatsOils: 2,
+        sodium: 2400,
+        caffeine: 60,
+        sweets: 1,
+        nutsSeedsLegumes: 2,
+        alcohol: 0
+      }
+    },
+    {
+      date: [2023, 3, 2],
+      data: {
+        grains: 4,
+        vegetables: 3,
+        fruits: 3,
+        meat: 7,
+        dairy: 3,
+        fatsOils: 2,
+        sodium: 2200,
+        caffeine: 80,
+        sweets: 0,
+        nutsSeedsLegumes: 3,
+        alcohol: 1
+      }
+    },
+    {
+        date: [2023, 3, 3],
+        data: {
+          grains: 3,
+          vegetables: 2,
+          fruits: 2,
+          meat: 6,
+          dairy: 2,
+          fatsOils: 2,
+          sodium: 2400,
+          caffeine: 60,
+          sweets: 1,
+          nutsSeedsLegumes: 2,
+          alcohol: 0
+        }
+      },
+      {
+        date: [2023, 3, 4],
+        data: {
+          grains: 4,
+          vegetables: 3,
+          fruits: 3,
+          meat: 7,
+          dairy: 3,
+          fatsOils: 2,
+          sodium: 2200,
+          caffeine: 80,
+          sweets: 0,
+          nutsSeedsLegumes: 3,
+          alcohol: 1
+        }
+      },
+      {
+        date: [2023, 3, 5],
+        data: {
+          grains: 3,
+          vegetables: 2,
+          fruits: 2,
+          meat: 6,
+          dairy: 2,
+          fatsOils: 2,
+          sodium: 2400,
+          caffeine: 60,
+          sweets: 1,
+          nutsSeedsLegumes: 2,
+          alcohol: 0
+        }
+      },
+      {
+        date: [2023, 3, 6],
+        data: {
+          grains: 4,
+          vegetables: 3,
+          fruits: 3,
+          meat: 7,
+          dairy: 3,
+          fatsOils: 2,
+          sodium: 2200,
+          caffeine: 80,
+          sweets: 0,
+          nutsSeedsLegumes: 3,
+          alcohol: 1
+        }
+      },
+      {
+        date: [2023, 3, 7],
+        data: {
+          grains: 3,
+          vegetables: 2,
+          fruits: 2,
+          meat: 6,
+          dairy: 2,
+          fatsOils: 2,
+          sodium: 2400,
+          caffeine: 60,
+          sweets: 1,
+          nutsSeedsLegumes: 2,
+          alcohol: 0
+        }
+      },
+      {
+        date: [2023, 3, 8],
+        data: {
+          grains: 4,
+          vegetables: 3,
+          fruits: 3,
+          meat: 7,
+          dairy: 3,
+          fatsOils: 2,
+          sodium: 2200,
+          caffeine: 80,
+          sweets: 0,
+          nutsSeedsLegumes: 3,
+          alcohol: 1
+        }
+      },
+      {
+        date: [2023, 3, 9],
+        data: {
+          grains: 3,
+          vegetables: 2,
+          fruits: 2,
+          meat: 6,
+          dairy: 2,
+          fatsOils: 2,
+          sodium: 2400,
+          caffeine: 60,
+          sweets: 1,
+          nutsSeedsLegumes: 2,
+          alcohol: 0
+        }
+      },
+      {
+        date: [2023, 3, 10],
+        data: {
+          grains: 4,
+          vegetables: 3,
+          fruits: 3,
+          meat: 7,
+          dairy: 3,
+          fatsOils: 2,
+          sodium: 2200,
+          caffeine: 80,
+          sweets: 0,
+          nutsSeedsLegumes: 3,
+          alcohol: 1
+        }
+      },
+      {
+        date: [2023, 3, 11],
+        data: {
+          grains: 3,
+          vegetables: 2,
+          fruits: 2,
+          meat: 6,
+          dairy: 2,
+          fatsOils: 2,
+          sodium: 2400,
+          caffeine: 60,
+          sweets: 1,
+          nutsSeedsLegumes: 2,
+          alcohol: 0
+        }
+      },
+      {
+        date: [2023, 3, 12],
+        data: {
+          grains: 4,
+          vegetables: 3,
+          fruits: 3,
+          meat: 7,
+          dairy: 3,
+          fatsOils: 2,
+          sodium: 2200,
+          caffeine: 80,
+          sweets: 0,
+          nutsSeedsLegumes: 3,
+          alcohol: 1
+        }
+      },
+      {
+        date: [2023, 3, 13],
+        data: {
+          grains: 3,
+          vegetables: 2,
+          fruits: 2,
+          meat: 6,
+          dairy: 2,
+          fatsOils: 2,
+          sodium: 2400,
+          caffeine: 60,
+          sweets: 1,
+          nutsSeedsLegumes: 2,
+          alcohol: 0
+        }
+      },
+    //...more days
+  ]
+  export default days;
